9430fa4b9ff5009602ba289458862632562634d2
Dima <78157933+DmitriyIgnatev@users.noreply.github.com>
2022-10-23 13:41:16 +0300

    надеюсь теперь flake8 доволен

--- Task1/homepage/tests.py
+++ Task1/homepage/tests.py
@@ -9,7 +9,7 @@ class StaticURLTests(TestCase):
     def test_fail(self):
         response = Client().get('/home/123')
         self.assertEqual(response.status_code, 200)  # Провальный тест
-    
+
     def test_notfail(self):
         response = Client().get('/home/123')
         self.assertEqual(response.status_code, 404)
