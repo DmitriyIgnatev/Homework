49c6ab4b0c6ce18031401172e13a2569de37d5ae
Dima <78157933+DmitriyIgnatev@users.noreply.github.com>
2022-10-24 18:08:16 +0300

    flake8

--- Task1/catalog/tests.py
+++ Task1/catalog/tests.py
@@ -17,7 +17,7 @@ class StaticURLTests(TestCase):
     def test_not_normal_number_endpoint(self):
         response = Client().get('/catalog/1.2')
         self.assertEqual(response.status_code, 404)
-    
+
     def test_catalog_endpoint_more(self):
         response = Client().get('/catalog/')
         self.assertEqual(response.status_code, 200)
