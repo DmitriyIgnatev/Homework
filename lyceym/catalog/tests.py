a81f1a1737d2ad126e0b410cf3605cbe77d7bbc0
Dima <78157933+DmitriyIgnatev@users.noreply.github.com>
2022-10-26 18:07:45 +0300

    ,

--- lyceym/catalog/tests.py
+++ lyceym/catalog/tests.py
@@ -22,82 +22,82 @@ class StaticURLTests(TestCase):
         response = Client().get('/catalog/')
         self.assertEqual(response.status_code, 200)
 
-    def test_new_test_1(self):
+    def test_catalog_new_test_endpoint(self):
         response = Client().get('/catalog/0/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_2(self):
+    def test_catalog_new_tewrst_endpoint(self):
         response = Client().get('/catalog/-1/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_3(self):
+    def test_catalog_new_teast_endpoint(self):
         response = Client().get('/catalog/-1smth/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_4(self):
+    def test_catalog_new_teahst_endpoint(self):
         response = Client().get('/catalog/0d/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_5(self):
+    def test_catalog_new_tegst_endpoint(self):
         response = Client().get('/catalog/smth0/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_6(self):
+    def test_catalog_new_tedst_endpoint(self):
         response = Client().get('/catalog/smth-1/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_7(self):
+    def test_catalog_new_tesjt_endpoint(self):
         response = Client().get('/catalog/1.2/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_8(self):
+    def test_catalog_new_testc_endpoint(self):
         response = Client().get('/catalog/1.2smth/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_9(self):
+    def test_catalog_new_tesst_endpoint(self):
         response = Client().get('/catalog/smth1.2/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_11(self):
+    def test_catalog_new_tesgt_endpoint(self):
         response = Client().get('/catalog/smth-1.2/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_12(self):
+    def test_catalog_new_tehsst_endpoint(self):
         response = Client().get('/catalog/-1.2smth/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_13(self):
+    def test_catalog_new_tedfst_endpoint(self):
         response = Client().get('/catalog/12/2/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_14(self):
+    def test_catalog_new_teaskt_endpoint(self):
         response = Client().get('/catalog/12smth12/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_15(self):
+    def test_catalog_new_tedhst_endpoint(self):
         response = Client().get('/catalog/-12smth-12/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_16(self):
+    def test_catalog_new_tejst_endpoint(self):
         response = Client().get('/catalog/-1.2smth-1.2/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_17(self):
+    def test_catalog_new_tesgst_endpoint(self):
         response = Client().get('/catalog/1.2smth1.2/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_18(self):
+    def test_catalog_new_teshst_endpoint(self):
         response = Client().get('/catalog/1.2smth-1.2/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_19(self):
+    def test_catalog_new_tesqwt_endpoint(self):
         response = Client().get('/catalog/-123smth124/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_20(self):
+    def test_catalog_new_teerst_endpoint(self):
         response = Client().get('/catalog/-1.2smth1234/')
         self.assertEqual(response.status_code, 404)
 
-    def test_new_test_21(self):
+    def test_catalog_new_teqst_endpoint(self):
         response = Client().get('/catalog/-123smth1234/')
         self.assertEqual(response.status_code, 404)
