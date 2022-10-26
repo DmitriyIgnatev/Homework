48f7fe9b8d0546d73eff238e2a807cb5024e34d2
Dima <78157933+DmitriyIgnatev@users.noreply.github.com>
2022-10-26 18:12:08 +0300

    .

--- lyceym/catalog/tests.py
+++ lyceym/catalog/tests.py
@@ -22,82 +22,18 @@ class StaticURLTests(TestCase):
         response = Client().get('/catalog/')
         self.assertEqual(response.status_code, 200)
 
-    def test_catalog_new_test_endpoint(self):
+    def test_catalog_new_endpoint(self):
         response = Client().get('/catalog/0/')
         self.assertEqual(response.status_code, 404)
 
-    def test_catalog_new_tewrst_endpoint(self):
+    def test_catalog_new_minus_endpoint(self):
         response = Client().get('/catalog/-1/')
         self.assertEqual(response.status_code, 404)
 
-    def test_catalog_new_teast_endpoint(self):
-        response = Client().get('/catalog/-1smth/')
+    def test_catalog_new_numandcup_endpoint(self):
+        response = Client().get('/catalog/-1something/')
         self.assertEqual(response.status_code, 404)
 
-    def test_catalog_new_teahst_endpoint(self):
-        response = Client().get('/catalog/0d/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tegst_endpoint(self):
-        response = Client().get('/catalog/smth0/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tedst_endpoint(self):
-        response = Client().get('/catalog/smth-1/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tesjt_endpoint(self):
-        response = Client().get('/catalog/1.2/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_testc_endpoint(self):
-        response = Client().get('/catalog/1.2smth/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tesst_endpoint(self):
-        response = Client().get('/catalog/smth1.2/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tesgt_endpoint(self):
-        response = Client().get('/catalog/smth-1.2/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tehsst_endpoint(self):
-        response = Client().get('/catalog/-1.2smth/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tedfst_endpoint(self):
-        response = Client().get('/catalog/12/2/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_teaskt_endpoint(self):
-        response = Client().get('/catalog/12smth12/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tedhst_endpoint(self):
-        response = Client().get('/catalog/-12smth-12/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tejst_endpoint(self):
-        response = Client().get('/catalog/-1.2smth-1.2/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tesgst_endpoint(self):
-        response = Client().get('/catalog/1.2smth1.2/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_teshst_endpoint(self):
-        response = Client().get('/catalog/1.2smth-1.2/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_tesqwt_endpoint(self):
-        response = Client().get('/catalog/-123smth124/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_teerst_endpoint(self):
-        response = Client().get('/catalog/-1.2smth1234/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_new_teqst_endpoint(self):
-        response = Client().get('/catalog/-123smth1234/')
+    def test_catalog_new_zero_endpoint(self):
+        response = Client().get('/catalog/0something/')
         self.assertEqual(response.status_code, 404)
--- lyceym/catalog/urls.py
+++ lyceym/catalog/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path, re_path
 from catalog import views
 
