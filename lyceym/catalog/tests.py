63c1c4a4c633dbf6b548c7bbdfeaaaf3c998c407
Dima <78157933+DmitriyIgnatev@users.noreply.github.com>
2022-10-26 18:41:01 +0300

    +test

--- lyceym/catalog/tests.py
+++ lyceym/catalog/tests.py
@@ -37,3 +37,19 @@ class StaticURLTests(TestCase):
     def test_catalog_new_zero_endpoint(self):
         response = Client().get('/catalog/0something/')
         self.assertEqual(response.status_code, 404)
+
+    def test_catalog_new_number_endpoint(self):
+        response = Client().get('/catalog/something-124/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_catalog_new_nor_endpoint(self):
+        response = Client().get('/catalog/1.2/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_catalog_new_nr_endpoint(self):
+        response = Client().get('/catalog/-1.2/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_catalog_new_r_endpoint(self):
+        response = Client().get('/catalog/-1.2ggg/')
+        self.assertEqual(response.status_code, 404)
